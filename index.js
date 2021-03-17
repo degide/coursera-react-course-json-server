f743172459804869c43c3ad631d3d467e0185d87
Harerimana Egide <egideharerimana085@gmail.com>
2021-03-17 12:16:12 +0200

    rebuilt server and added index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const jsonServer = require('json-server')
+var demodata = require('./db.json')
+
+// Avoid CORS issue
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const router = jsonServer.router(demodata) // Express router
+const server = jsonServer.create()       // Express server
+
+server.use('/', express.static(path.join(__dirname, 'public')))
+server.use(router)
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, "http://localhost", ()=> {
+    console.log(`[${new Date()}] Server started on port: ${PORT}`);
+})
