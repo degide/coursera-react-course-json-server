3a6b7ca788a1930239490ae25af96402ae5033b7
Harerimana Egide <egideharerimana085@gmail.com>
2021-03-17 13:24:36 +0200

    solved directory issues

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const express = require('express')
 const jsonServer = require('json-server')
 var demodata = require('./db.json')
 
-
 const router = jsonServer.router(demodata) // Express router
-const server = jsonServer.create()       // Express server
+const app = express()       // Express server
+
 // Avoid CORS issue
-server.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-server.use('/', express.static(path.join(__dirname, 'public')))
-server.use(router)
+app.use(router)
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3001
-server.listen(PORT, "http://localhost", ()=> {
+app.listen(PORT, ()=> {
     console.log(`[${new Date()}] Server started on port: ${PORT}`);
 })
