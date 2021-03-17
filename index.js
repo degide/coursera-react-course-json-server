1b534c2abe205e9e9f5d37ef005c5b1d88bdc95c
Harerimana Egide <egideharerimana085@gmail.com>
2021-03-17 12:17:48 +0200

    resolved potential issue

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express')
 const jsonServer = require('json-server')
 var demodata = require('./db.json')
 
+
+const router = jsonServer.router(demodata) // Express router
+const server = jsonServer.create()       // Express server
 // Avoid CORS issue
 server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-const router = jsonServer.router(demodata) // Express router
-const server = jsonServer.create()       // Express server
 
 server.use('/', express.static(path.join(__dirname, 'public')))
 server.use(router)
